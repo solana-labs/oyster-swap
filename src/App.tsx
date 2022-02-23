29f92205ba5b3bc901f325020c815974c8d5616a
crownholder <keesnetflix.1@gmail.com>
2022-02-23 22:36:59 +0000

    4th commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ function App() {
     <div className="App">
       <div className="Banner">
         <div className="Banner-description">
-          Swap is unaudited software. Use at your own risk.
+          Anti SWap 
         </div>
       </div>
       <Routes />
       <div className="social-buttons">
         <GitHubButton
-          href="https://github.com/project-serum/oyster-swap"
+          href="https://github.com/crownholder"
           data-color-scheme="no-preference: light; light: light; dark: light;"
           data-icon="octicon-star"
           data-size="large"
@@ -24,10 +24,10 @@ function App() {
           Star
         </GitHubButton>
         <GitHubButton
-          href="https://github.com/project-serum/oyster-swap/fork"
+          href="https://github.com/crownholder"
           data-color-scheme="no-preference: light; light: light; dark: light;"
           data-size="large"
-          aria-label="Fork project-serum/oyster-swap on GitHub"
+          aria-label="Anti Git Hub Account "
         >
           Fork
         </GitHubButton>
--- src/components/exchange.tsx
+++ src/components/exchange.tsx
@@ -38,7 +38,7 @@ export const ExchangeView = (props: {}) => {
       <div className="App-Bar-right">
         <Button type="text" size="large" style={{ color: "#2abdd2" }}>
           <a
-            href={"https://dex.projectserum.com"}
+            href={"https://www.antidote.world"}
             target="_blank"
             rel="noopener noreferrer"
           >
